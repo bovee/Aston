f466bf22279d4a3db8f018d121fefd3ff2d34e90
Roderick Bovee <bovee@fas.harvard.edu>
2012-01-09 18:41:25 -0500

    Added prelimary GPL info.

--- aston.py
+++ aston.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
+
+#    Copyright 2011, 2012 Roderick Bovee
+#   
+#    This file is part of Aston.
+#
+#    Aston is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Aston is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Aston.  If not, see <http://www.gnu.org/licenses/>.
+
 '''Loads and runs the Aston application.'''
 #pylint: disable=C0103
 
