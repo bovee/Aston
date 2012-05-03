6075a120412a7a1bf48c25e4ff3b2b46b0209a3b
Roderick Bovee <bovee@fas.harvard.edu>
2012-05-03 19:01:23 -0400

    Updated version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 options = {
     'name':'Aston',
-    'version':'0.2.0',
+    'version':'0.2.2',
     'description':'Mass/UV Spectral Analysis Program',
     'author':'Roderick Bovee',
     'author_email':'bovee@fas.harvard.edu',
