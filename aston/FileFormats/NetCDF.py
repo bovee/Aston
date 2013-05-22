58e870f2cbc13f792556424779331b9637652ba1
Roderick Bovee <rbovee@gmail.com>
2013-05-22 15:18:40 -0400

    New NetCDF stuff.

--- aston/FileFormats/NetCDF.py
+++ aston/FileFormats/NetCDF.py
@@ -43,10 +43,47 @@ class NetCDF(Datafile):
 
 
 def write_netcdf(dt, filename):
-    #f = NetCDFFile(filename, 'w')
-    #f.createVariable('scan_acquisition_time', ??, ??)
-    #f.createVariable('point_count', ??, ??)
-    #f.createVariable('mass_values', ??, ??)
-    #f.createVariable('intensity_values', ??, ??)
+    #FIXME: still a lot of issues here
+    f = NetCDFFile(filename, 'w')
 
-    raise NotImplementedError
+    f.createDimension('_2_byte_string', 2)
+    f.createDimension('_4_byte_string', 4)
+    f.createDimension('_8_byte_string', 8)
+    f.createDimension('_16_byte_string', 16)
+    f.createDimension('_32_byte_string', 32)
+    f.createDimension('_64_byte_string', 64)
+    f.createDimension('_128_byte_string', 128)
+    f.createDimension('_255_byte_string', 255)
+    f.createDimension('error_number', 1)
+    f.flush()
+
+    f.dataset_completeness = 'C1'  # TODO: save peaks too? ('C1+C2')
+    f.netcdf_revision = '2.3.2'
+    f.languages = 'English'
+    f.flush()
+
+    f.experiment_title = dt.info.get('name', ' ')
+    f.operator_name = dt.info.get('r-opr', ' ')
+    # TODO: wrong format for injection_date_time_stamp
+    f.injection_date_time_stamp = dt.info.get('r-date', ' ')
+    f.company_method_id = dt.info.get('m', ' ')
+    f.sample_name = dt.info.get('r-smp', ' ')
+    f.flush()
+
+    f.createDimension('scan_number', len(dt.data.times))
+    v = f.createVariable('scan_acquisition_time', '>d', ('scan_number',))
+    v[:] = dt.data.times.astype('d')
+    v = f.createVariable('total_intensity', '>d', ('scan_number',))
+    v[:] = dt.data.data.sum(axis=1).astype('d')
+    v = f.createVariable('point_count', '>i', ('scan_number',))
+    v[:] = np.sum(dt.data.data != 0, axis=1).astype('i')
+    f.flush()
+
+    f.createDimension('point_number', np.sum(dt.data.data != 0))
+    stretch_t = np.resize(dt.data.times, dt.data.data.T.shape).T
+    v = f.createVariable('mass_values', '>f', ('point_number',))
+    v[:] = stretch_t[dt.data.data != 0]
+    v = f.createVariable('intensity_values', '>f', ('point_number',))
+    v[:] = dt.data.data[dt.data.data != 0]
+
+    f.close()
