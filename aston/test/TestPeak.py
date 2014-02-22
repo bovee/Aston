33a809491a50d329112a717e6f8226ba55f0d290
Roderick Bovee <rbovee@gmail.com>
2014-02-22 12:00:55 -0500

    Started trying to write a test.

--- aston/test/TestPeak.py
+++ aston/test/TestPeak.py
@@ -1,1 +1,19 @@
- 
+import pytest
+import numpy as np
+from aston.trace.Trace import AstonSeries
+from aston.peaks.Peak import Peak, PeakComponent
+
+
+@pytest.fixture(params=['box'])
+def peak(request):
+    peak_name = request.param
+    if peak_name == 'box':
+        info = {}
+        trace = AstonSeries(np.arange(10), np.ones(10), name=1)
+        baseline = AstonSeries([0, 9], [0, 0], name=1)
+
+    return Peak(peak_name, components=PeakComponent(info, trace, baseline))
+
+
+def test_area(peak):
+    assert peak.area() == peak.area(1)
